fbbb1fd1a0c0b5df51afc5c925d89e65fd852b9a
seongjunnoh <joon1754@naver.com>
2025-03-26 11:08:50 +0900

    스프링 프로젝트 초기 세팅

---
+++ src/test/java/rediclaim/couponbackend/CouponBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rediclaim.couponbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
