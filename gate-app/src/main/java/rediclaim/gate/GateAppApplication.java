25727d09b3e565e57f04a741a7dc6e1e4a51e1c7
seongjunnoh <joon1754@naver.com>
2026-01-18 18:50:04 +0900

    feat : 선착순 시스템의 대기열을 담당하는 gate 모듈 추가

---
+++ gate-app/src/main/java/rediclaim/gate/GateAppApplication.java
@@ -0,0 +1,11 @@
+package rediclaim.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateAppApplication.class, args);
+    }
+}
